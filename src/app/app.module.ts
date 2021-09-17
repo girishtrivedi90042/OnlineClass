9e0becbde2bd0e9a04969ef0123d6940813852dd
girishtrivedi90042 <girish.trivedi90042@gmail.com>
2021-09-18 00:14:01 +0530

    6 files modified, 14 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StudentsComponent } from './students/students.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { CpopupComponent } from './cpopup/cpopup.component';
+// import { StudentmodelComponent } from './studentmodel/studentmodel.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [
+    AppComponent,
+    StudentsComponent,
+    TeachersComponent,
+    CpopupComponent,
+    // StudentmodelComponent
+  ],
+  imports:      [ BrowserModule,AppRoutingModule, FormsModule ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/students/students.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+// import studentdata from '../data/STUDENT.json';
+
+interface Student{
+  first_name : String;
+  last_name : String;
+  Roll_Number : number;
+  Date_Of_Birth : string;
+  Class : string;
+}
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+  router: any;
+  constructor(){}
+  
+  // students : Student[] = studentdata;
+
+  ngOnInit(): void {
+  }
+
+
+  navigate():void {
+    //console.log(studentdata)
+    this.router.navigate(['./StudentmodelComponent']);
+  }
+
+}
